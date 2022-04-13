e9d07434df9b6dad8c7e02b5e12b9323fdd406ff
Nemo <thereisnotruth12@gmail.com>
2022-04-13 20:56:35 +0900

    Sidebar Profile

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -4,7 +4,13 @@ export default class Sidebar extends Component {
   template() {
     return `
       <div id="sidebar-header">
-        profile
+        <img id="sidebar-profile-img" src="src/img/profile.png"> 
+        <div id="sidebar-profile-name">
+          고태진
+        </div>
+        <div id="sidebar-profile-intro">
+          개발자
+        </div>
       </div>
       <div id="sidebar-nav">
         menu
