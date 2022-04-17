98efd414d3c17c36afa2556d9336cde25bd6c0af
Nemo <thereisnotruth12@gmail.com>
2022-04-17 23:57:13 +0900

    File Read Test

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const path = require("path");
 const fs = require("fs");
+const hljs = require("highlight.js");
+const md = require("markdown-it")({
+  html: false,
+  xhtmlOut: false,
+  breaks: false,
+  langPrefix: "language-",
+  linkify: true,
+  typographer: true,
+  quotes: "“”‘’",
+  highlight: function(str, lang) {
+    if(lang && hljs.getLanguage(lang)) {
+      try {
+        return (
+          '<pre class="hljs"><code>' + 
+          hljs.highlight(lang, str, true).value +
+          "</code></pre>"
+        );
+      } catch (__) {}
+    }
+    return (
+      '<pre class="hljs"><code>' +
+      md.utils.escapeHtml(str) +
+      "</code></pre>"
+    );
+  }
+});
 
 const directoryPath = path.join(__dirname, "posts");
 console.log(directoryPath);
 
 const contentFiles = fs.readdirSync(directoryPath);
 console.log(contentFiles);
+
+contentFiles.map((file) => {
+  const body = fs.readFileSync(`./posts/${file}`, "utf8");
+  const convertedBody = md.render(body);
+  console.log(convertedBody);
+});
