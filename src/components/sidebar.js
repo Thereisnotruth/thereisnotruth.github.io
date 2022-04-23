b46fb57277f40abfe72baebac31cbfd50a80b22a
Nemo <thereisnotruth12@gmail.com>
2022-04-23 23:13:30 +0900

    sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,42 +1,61 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import sidebarStyles from "../styles/sidebar.css"
 
 const Sidebar = () => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <nav>
+    <div id="sidebar-header">
+      <img id="sidebar-profile-img" src="static/img/profile.png" /> 
+      <div id="sidebar-profile-name">
+        고태진
+      </div>
+      <div id="sidebar-profile-intro">
+        개발자
+      </div>
     </div>
-  </header>
+    <hr color="#eeeeee" />
+    <div id="sidebar-nav">
+      <div class="sidebar-nav-item">
+        <img class="sidebar-nav-icon" src="static/img/house-solid.svg" />
+        Home
+      </div>
+      <div class="sidebar-nav-item">
+        <img class="sidebar-nav-icon" src="static/img/pen-to-square-solid.svg" />
+        <a href="/blog" data-link>Blog</a>
+      </div>
+      <div class="sidebar-nav-item">
+        <img class="sidebar-nav-icon" src="static/img/box-archive-solid.svg" />
+        Archieve
+      </div>
+      <div class="sidebar-nav-item">
+        <img class="sidebar-nav-icon" src="static/img/gear-solid.svg" />
+        Projects
+      </div>
+      <div class="sidebar-nav-item">
+        <img class="sidebar-nav-icon" src="static/img/link-solid.svg" />
+        Links
+      </div>
+      <div class="sidebar-nav-item">
+        <img class="sidebar-nav-icon" src="static/img/user-solid.svg" />
+        About
+      </div>
+    </div>
+    <div id="sidebar-link">
+      <div id="sidebar-link-header">
+       Contact 
+      </div>
+      <div id="sidebar-link-item">
+        <img class="sidebar-link-icon" src="static/img/github-brands.svg" />
+      </div>
+      <div id="sidebar-link-item">
+        <img class="sidebar-link-icon" src="static/img/linkedin-in-brands.svg" />
+      </div>
+    </div>
+    <div id="sidebar-mod">
+      <img id="sidebar-mod-icon" src="static/img/moon-solid.svg" />
+    </div>
+  </nav>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Sidebar;
