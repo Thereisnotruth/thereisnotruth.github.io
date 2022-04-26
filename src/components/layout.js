104eb6b50a9f15c0e9e37d12012400a13bf73bb2
Nemo <thereisnotruth12@gmail.com>
2022-04-26 23:34:34 +0900

    searchbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,12 +3,14 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Sidebar from "./sidebar"
+import Searchbar from "./searchbar"
 import "../styles/layout.css"
 
 const Layout = ({ children }) => {
   return (
     <div id="layout">
       <Sidebar />
+      <Searchbar />
       <div style={{ float: `left` }}>
         <main>{children}</main>
       </div>
---
+++ src/components/searchbar.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import "../styles/searchbar.css"
+
+const Searchbar = () => (
+  <div className="searchbar">
+    <div className="searchbar-button">
+      <StaticImage className="searchbar-icon" src="../images/magnifying-glass-solid.svg" />
+    </div>
+  </div>
+)
+
+export default Searchbar;
