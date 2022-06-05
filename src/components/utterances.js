790ead960e5e6d1a7820c63b867d4f863695331c
Nemo <thereisnotruth12@gmail.com>
2022-06-06 01:13:44 +0900

    utterances

--- src/components/utterances.js
+++ src/components/utterances.js
@@ -1,34 +1,26 @@
 import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Seo from "./seo"
-import Sidebar from "./sidebar"
-import Searchbar from "./searchbar"
 
 const Utterances = ({ repo, theme }) => {
+  console.log("Test")
   const containerRef = React.createRef();
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     const utterances = document.createElement("script");
     const attributes = {
       src: "https://utteranc.es/client.js",
       repo,
-      "issue-term": "title",
-      label: "comment",
+      "issue-term": "pathname",
+      label: "comments",
       theme,
       crossOrigin: "anonymous",
       async: "true",
     };
     Object.entries(attributes).forEach(([key, value]) => {
       utterances.setAttribute(key, value);
-    });
+    })
     containerRef.current.appendChild(utterances);
   }, [repo, theme]);
-  return (
-    <div ref={containerRef}>
-    </div>
-  )
-}
+  return <div  ref={containerRef} />;
+};
 
 export default Utterances
 
