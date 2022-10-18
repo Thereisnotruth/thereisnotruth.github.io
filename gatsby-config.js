2cff689103a1a6c43694a3157e2cde8de5a95ec9
고래는서서잔다 <thereisnotruth12@gmail.com>
2022-10-18 08:47:22 +0000

    post spring

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 600,
+              maxWidth: 300,
             },
           },
 			{
