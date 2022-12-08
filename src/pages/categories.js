3f13dd6ca0f7c1717241a25bea82132256f1bae0
고태진 <thereisnotruth12@gmail.com>
2022-12-09 05:20:15 +0900

    categories

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -6,55 +6,44 @@ import Layout from "../components/layout"
 import "../styles/list_card.css"
 
 const CategoriesPage = ({ data }) => {
-  const [curCategory, setCurCategory] = React.useState("none");
-  const categories = {};
+  const categories = []
 
-  data.allMdx.nodes.sort((a, b) => {
-    if (a.frontmatter.category > b.frontmatter.category) {
-      return 1;
-    } else if (a.frontmatter.categroy < b.frontmatter.category) {
-      return -1;
-    } else {
-      return 0;
+  const directories = data.allDirectory.edges
+  for (let i = 0; i < directories.length; i++) {
+    if (
+      directories[i].node.name[0] >= "0" &&
+      directories[i].node.name[0] <= "9"
+    ) {
+      continue
     }
-  });
-
-  data.allMdx.nodes.map((node) => {
-    if (categories[node.frontmatter.category] === undefined) {
-      categories[node.frontmatter.category] = [node];
-    } else {
-      categories[node.frontmatter.category].push(node);
-    }
-  });
-
-  const showCategories = () => {
-    const ret = [];
-    for (let category in categories) {
-      ret.push(
-        <Link className="link" to={category} key={category}>
-          <div className="list-card">
-            <h2>{category}</h2>
-          </div>
-        </Link>
-      );
-    }
-    return ret;
+    categories.push(directories[i].node.name)
   }
 
-  return (
-    <Layout>
-      {showCategories()}
-    </Layout>
-  );
+  console.log(categories)
+  const showCategories = () => {
+    const ret = []
+    for (let i = 0; i < categories.length; i++) {
+      ret.push(
+        <Link className="link" to={categories[i]} key={categories[i]}>
+          <div className="list-card">
+            <h2>{categories[i]}</h2>
+          </div>
+        </Link>
+      )
+    }
+    return ret
+  }
+
+  return <Layout>{showCategories()}</Layout>
 }
 
 export const query = graphql`
   query CategoryQuery {
-    allMdx(sort: {fields: frontmatter___idx, order: DESC}) {
-      nodes {
-        id
-        frontmatter {
-          category
+    allDirectory {
+      edges {
+        node {
+          id
+          name
         }
       }
     }
